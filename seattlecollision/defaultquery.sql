2f0dd965eb8f2bfb88cba4e22eb3ad062ab6f700
Dan White <dan.white105@gmail.com>
2018-05-21 14:12:08 -0700

    Updated During, After default queries

--- seattlecollision/defaultquery.sql
+++ seattlecollision/defaultquery.sql
@@ -1,31 +1,28 @@
 BEFORE
 
-SELECT build_id, build_lat, build_long, build_start_dt, build_end_dt, 
+SELECT build_id, build_lat, build_long, build_start_dt, build_end_dt,
 count(coll_id) as num_collisions
 FROM radius
 WHERE build_start_dt LIKE '%2015%' 
-	AND radius < 1000
-	AND date(coll_id) BETWEEN date('build_start_dt', '-6 months') AND date('build_start_dt')
-GROUP BY build_id, build_lat, build_long, build_start_dt, build_end_dt
+	AND date(coll_dt) BETWEEN date(build_start_dt, '-6 months') AND date(build_start_dt)
+	AND radius < 1500
+GROUP BY build_id, build_lat, build_long, build_start_dt, build_end_dt;
 
 DURING
 
-
-
-
-
+SELECT build_id, build_lat, build_long, build_start_dt, build_end_dt,
+count(coll_id) as num_collisions
+FROM radius
+WHERE build_start_dt LIKE '%2015%' 
+	AND date(coll_dt) BETWEEN date(build_start_dt) AND date(build_end_dt)
+	AND radius < 1500
+GROUP BY build_id, build_lat, build_long, build_start_dt, build_end_dt;
 
 AFTER
 
 SELECT build_id, build_lat, build_long, build_start_dt, build_end_dt, count(coll_id) as num_collisions
 FROM radius
-WHERE build_start_dt LIKE '%2016%' 
-	AND date(coll_dt) BETWEEN date(build_start_dt, '-6 months') AND date(build_start_dt)
+WHERE build_start_dt LIKE '%2015%' 
+	AND date(coll_dt) BETWEEN date(build_end_dt) AND date (build_end_dt, '+6 months')
+	AND radius < 1500
 GROUP BY build_id, build_lat, build_long, build_start_dt, build_end_dt;
-
-select count(*) from (
-select build_id, build_lat, build_long, build_end_dt, build_start_dt, count(coll_id) as num_collisions
-from radius
-where build_start_dt like '%2013%' 
-and date(coll_dt) between date(build_start_dt, '-6 months') and date(build_start_dt)
-group by build_id, build_lat, build_long, build_end_dt, build_start_dt);
