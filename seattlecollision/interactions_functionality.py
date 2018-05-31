f568457157e8e8dbdbd699327c988492ead0bf8a
Alyssa Goodrich <alcgood@uw.edu>
2018-05-31 11:56:23 -0700

    added additional unit tests

--- seattlecollision/interactions_functionality.py
+++ seattlecollision/interactions_functionality.py
@@ -27,16 +27,20 @@ def generate_connection(data_directory):
 
 
 
-def generate_categories(cols_needed):
+def generate_categories(cols_needed, data_directory="data/Collidium"):
     """
     TODO Insert docstring
     """
-
+    if not os.path.exists(str(data_directory)):
+        raise ValueError(str((data_directory) +" is not a valid path"))
+    true_set = set(['b_category', 'base_year', 'c_severity', 'c_type'])
+    if not set(list(cols_needed)) == true_set:
+        raise IndexError("cols_needed must only contain the fields:", true_set)
     categories = list()
     i = 0
     for col in cols_needed:
         query_string = str("select distinct " + col + " from collidium_data")
-        sql_cursor = generate_connection("data/Collidium")
+        sql_cursor = generate_connection(data_directory)
         sql_cursor.execute(query_string)
         temp = pd.DataFrame(sql_cursor.fetchall())
         temp = temp.values.tolist()
@@ -46,71 +50,85 @@ def generate_categories(cols_needed):
         i += 1
     return categories[0], categories[1], categories[2], categories[3]
 
-def generate_table(query):
+def generate_table(query, data_directory="data/Collidium"):
     """
     TODO Insert docstring
     """
-    sql_cursor = generate_connection("data/Collidium")
+    if not os.path.exists(str(data_directory)):
+        raise ValueError(str((data_directory) +" is not a valid path"))
+    sql_cursor = generate_connection(data_directory)
     sql_cursor.execute(query)
     temp = pd.DataFrame(sql_cursor.fetchall())
     temp.columns = list(map(lambda x: x[0], sql_cursor.description))
     return temp
 
-def build_type_interact(building_category):
+def build_type_interact(building_category, data_directory="data/Collidium"):
     """
     TODO Insert docstring
     """
+    if not os.path.exists(str(data_directory)):
+        raise ValueError(str((data_directory) +" is not a valid path"))
     query_builder = CollidiumQuery()
     query_builder.set_b_category(building_category)
     whole_query = query_builder.get_qstring()
-    mapping_data = generate_table(whole_query)
+    mapping_data = generate_table(whole_query, data_directory)
     return draw_markers.place_maps(mapping_data)
 
-def year_int_interact(building_year, collision_interval):
+def year_int_interact(building_year, collision_interval, data_directory="data/Collidium"):
     """
     TODO Insert docstring
     """
+    if not os.path.exists(str(data_directory)):
+        raise ValueError(str((data_directory) +" is not a valid path"))
     query_builder = CollidiumQuery()
     query_builder.set_base_year(building_year)
     query_builder.set_duration(collision_interval)
     whole_query = query_builder.get_qstring()
-    mapping_data = generate_table(whole_query)
+    mapping_data = generate_table(whole_query, data_directory)
     return draw_markers.place_maps(mapping_data)
 
-def collision_severity_interact(collision_severity):
+def collision_severity_interact(collision_severity, data_directory="data/Collidium"):
     """
     TODO Insert docstring
     """
+    if not os.path.exists(str(data_directory)):
+        raise ValueError(str((data_directory) +" is not a valid path"))
     query_builder = CollidiumQuery()
     query_builder.set_c_severity(collision_severity)
     whole_query = query_builder.get_qstring()
-    mapping_data = generate_table(whole_query)
+    mapping_data = generate_table(whole_query, data_directory)
     return draw_markers.place_maps(mapping_data)
 
-def collision_type_interact(collision_type):
+def collision_type_interact(collision_type, data_directory="data/Collidium"):
     """
     TODO Insert docstring
     """
+    if not os.path.exists(str(data_directory)):
+        raise ValueError(str((data_directory) +" is not a valid path"))
     query_builder = CollidiumQuery()
     query_builder.set_c_type(collision_type)
     whole_query = query_builder.get_qstring()
-    mapping_data = generate_table(whole_query)
+    mapping_data = generate_table(whole_query, data_directory)
     return draw_markers.place_maps(mapping_data)
 
-def radius_interact(radius_from_building):
+def radius_interact(radius_from_building, data_directory="data/Collidium"):
     """
     TODO Insert docstring
     """
+    if not os.path.exists(str(data_directory)):
+        raise ValueError(str((data_directory) +" is not a valid path"))
     query_builder = CollidiumQuery()
     query_builder.set_radius(radius_from_building)
     whole_query = query_builder.get_qstring()
-    mapping_data = generate_table(whole_query)
+    mapping_data = generate_table(whole_query, data_directory)
     return draw_markers.place_maps(mapping_data)
 
-def all_factor_interact(building_category, building_year, collision_interval, collision_severity, collision_type, radius_from_building): #pylint: disable=line-too-long,too-many-arguments
+def all_factor_interact(building_category, building_year, collision_interval, collision_severity, collision_type, radius_from_building, data_directory="data/Collidium"): #pylint: disable=line-too-long,too-many-arguments
     """
     TODO Insert docstring
     """
+    if not os.path.exists(str(data_directory)):
+        raise ValueError(str((data_directory) +" is not a valid path"))
     query_builder = CollidiumQuery()
     query_builder.set_b_category(building_category)
     query_builder.set_base_year(building_year)
@@ -119,5 +137,5 @@ def all_factor_interact(building_category, building_year, collision_interval, co
     query_builder.set_c_type(collision_type)
     query_builder.set_radius(radius_from_building)
     whole_query = query_builder.get_qstring()
-    mapping_data = generate_table(whole_query)
+    mapping_data = generate_table(whole_query, data_directory)
     return draw_markers.place_maps(mapping_data)
--- seattlecollision/tests/test_interactions_functionality.py
+++ seattlecollision/tests/test_interactions_functionality.py
@@ -9,16 +9,17 @@ import unittest
 sys.path.append('../')
 import interactions_functionality as int_func #pylint: disable=wrong-import-position
 
-TEST_DB = int_func.generate_connection("../data/Collidium")
-TEST_DB.execute("select * from collidium_data")
-TEST_DF = TEST_DB.fetchall()
+TEST_CONNECTION = int_func.generate_connection("../data/Collidium")
+TEST_DF = int_func.generate_table("select * from collidium_data", data_directory="../data/Collidium") #pylint: disable=line-too-long
 
-class ConnectionTests(unittest.TestCase):
+
+
+class TestGenerateConnections(unittest.TestCase):
     '''Conducts test on the performance of the generate_connections function.
-    
+
     This class tests three functions in the interactions_functionality module.
-    These include the generate_connection function, the generate_categories 
-    function and the generate_table function. The other functions in this module 
+    These include the generate_connection function, the generate_categories
+    function and the generate_table function. The other functions in this module
     (those ending in _interact) are not tested in this module because they generate
     elements of the user interface that cannot be tested by unit tests.
 
@@ -34,17 +35,19 @@ class ConnectionTests(unittest.TestCase):
     '''
 
     def test_col_names(self):
-        '''Tests whether function output has correct column names.'''
+        '''Tests whether function connects to DB with the correct column names.'''
         true_cols = set(['b_category', 'b_end_dt', 'b_id', 'b_lat', 'b_long', 'b_start_dt', 'base_year', 'c_dt', 'c_id', 'c_lat', 'c_long', 'c_severity', 'c_type', 'coll_after', 'coll_before', 'coll_days_from_build', 'coll_during', 'radius']) #pylint: disable=line-too-long
-        TEST_DB.execute('PRAGMA TABLE_INFO({})'.format('collidium_data'))
-        test_cols = TEST_DB.execute('PRAGMA TABLE_INFO({})'.format('collidium_data'))
-        test_cols = set([tup[1] for tup in TEST_DB.fetchall()])
+        TEST_CONNECTION.execute('PRAGMA TABLE_INFO({})'.format('collidium_data'))
+        test_cols = TEST_CONNECTION.execute('PRAGMA TABLE_INFO({})'.format('collidium_data'))
+        test_cols = set([tup[1] for tup in TEST_CONNECTION.fetchall()])
         test_cols = set(test_cols)
         self.assertTrue(test_cols == true_cols)
 
     def test_data_table_size(self):
-        '''Ensures function returns output with at least 10,000 rows.'''
-        self.assertTrue(len(TEST_DF) > 10000)
+        '''Ensures function connects to DB with at least 10,000 rows.'''
+        TEST_CONNECTION.execute('select count(*) from collidium_data')
+        table_size = TEST_CONNECTION.fetchall()[0][0]
+        self.assertTrue(table_size > 10000)
 
 
     def test_invalid_path_error(self):
@@ -52,5 +55,82 @@ class ConnectionTests(unittest.TestCase):
         self.assertRaises(Exception, int_func.generate_connection, 'badPath')
 
 
-SUITE = unittest.TestLoader().loadTestsFromTestCase(ConnectionTests)
+SUITE = unittest.TestLoader().loadTestsFromTestCase(TestGenerateConnections)
+_ = unittest.TextTestRunner().run(SUITE)
+
+class TestGenerateCategories(unittest.TestCase):
+    '''Conducts test on the performance of the generate_connections function.
+
+    This class tests three functions in the interactions_functionality module.
+    These include the generate_connection function, the generate_categories
+    function and the generate_table function. The other functions in this module
+    (those ending in _interact) are not tested in this module because they generate
+    elements of the user interface that cannot be tested by unit tests.
+
+    This class conducts three tests, for the pupose of validating the functionality
+    of the interactions_functionality module. Each test is conducted within a function, as
+    follows:
+
+    Functions:
+       TO DO:
+
+    Returns: A human readable string that provides a summary of results of the five tests
+        conducted within this class.
+    '''
+
+    def test_cols_needed(self):
+        """Tests that invalid input raises an IndexError"""
+        bad_cols_needed = ["bad_category", "base_year", "c_severity", "c_type"]
+        self.assertRaises(IndexError, int_func.generate_categories, bad_cols_needed, data_directory="../data/Collidium") #pylint: disable=line-too-long
+
+    def test_output(self):
+        '''Ensures function outputs the correct lists.'''
+        cols_needed = ["b_category", "base_year", "c_severity", "c_type"]
+        test_categories = int_func.generate_categories(cols_needed, data_directory="../data/Collidium") #pylint: disable=line-too-long
+        true_output = (['All', 'COMMERCIAL', 'MULTIFAMILY', 'INDUSTRIAL', 'INSTITUTIONAL', 'SINGLE FAMILY / DUPLEX'], ['All', 2016, 2015, 2017, 2014, 2013], ['All', 'Property Damage Only', 'Injury', 'Serious Injury', 'Fatality'], ['All', 'Vehicle Only', 'Bike/Pedestrian']) #pylint: disable=line-too-long
+        self.assertTrue(test_categories == true_output)
+
+
+    def test_invalid_path_error(self):
+        '''Tests whether an invalid path raises a ValueError exception.'''
+        self.assertRaises(Exception, int_func.generate_categories, 'badPath')
+
+
+SUITE = unittest.TestLoader().loadTestsFromTestCase(TestGenerateCategories)
+_ = unittest.TextTestRunner().run(SUITE)
+
+
+class TestGenerateTable(unittest.TestCase):
+    '''Conducts test on the performance of the generate_connections function.
+
+    This class tests three functions in the interactions_functionality module.
+    These include the generate_connection function, the generate_categories
+    function and the generate_table function. The other functions in this module
+    (those ending in _interact) are not tested in this module because they generate
+    elements of the user interface that cannot be tested by unit tests.
+
+    This class conducts three tests, for the pupose of validating the functionality
+    of the interactions_functionality module. Each test is conducted within a function, as
+    follows:
+
+    Functions:
+       TO DO:
+
+    Returns: A human readable string that provides a summary of results of the five tests
+        conducted within this class.
+    '''
+    def test_col_names(self):
+        '''Tests whether function output has correct column names.'''
+        self.assertTrue(len(list(TEST_DF)) > 0)
+
+    def test_data_frame_size(self):
+        '''Ensures function returns output with at least 100 rows.'''
+        self.assertTrue(TEST_DF.shape[0] > 10000)
+
+    def test_invalid_path_error(self):
+        '''Tests whether an invalid path raises a ValueError exception.'''
+        self.assertRaises(Exception, int_func.generate_table, 'badPath')
+
+
+SUITE = unittest.TestLoader().loadTestsFromTestCase(TestGenerateTable)
 _ = unittest.TextTestRunner().run(SUITE)
