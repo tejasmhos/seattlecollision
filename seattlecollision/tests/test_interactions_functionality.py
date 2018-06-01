b10be3557a2e813926a586e88c8d03ace5ecac17
Alyssa Goodrich <alcgood@uw.edu>
2018-05-31 21:57:57 -0700

    added additional testing

--- seattlecollision/tests/test_interactions_functionality.py
+++ seattlecollision/tests/test_interactions_functionality.py
@@ -13,8 +13,8 @@ sys.path.append('seattlecollision/tests')
 
 import interactions_functionality as int_func #pylint: disable=wrong-import-position
 
-DATA_DIRECTORY = "seattlecollision/data/Collidium"
-# DATA_DIRECTORY = "../data/Collidium"
+# DATA_DIRECTORY = "seattlecollision/data/Collidium"
+DATA_DIRECTORY = "../data/Collidium"
 
 TEST_CONNECTION = int_func.generate_connection(DATA_DIRECTORY)
 TEST_DF = int_func.generate_table("select * from collidium_data", data_directory=DATA_DIRECTORY) #pylint: disable=line-too-long
@@ -166,39 +166,72 @@ class TestInteractFunctions(unittest.TestCase):
     Returns: A human readable string that provides a summary of results of the five tests
         conducted within this class.
     '''
-    # def test_col_names(self):
-    #     '''Tests whether function output has correct column names.'''
-    #     self.assertTrue(len(list(TEST_DF)) > 0)
 
     def test_build_interact(self):
         """todo: write doc string"""
         try:
             int_func.build_type_interact(building_category="All", data_directory=DATA_DIRECTORY)
-        except ExceptionType: #pylint: disable=undefined-variable
+        except RuntimeError:
             self.fail("build_type_interact raised ExceptionType unexpectedly.")
 
     def test_year_int_interact(self):
         """todo: write doc string"""
         try:
             int_func.year_int_interact(building_year=2016, collision_interval=12, data_directory=DATA_DIRECTORY) #pylint: disable=line-too-long
-        except ExceptionType: #pylint: disable=undefined-variable
+        except RuntimeError:
             self.fail("year_int_interact raised ExceptionType unexpectedly.")
 
-    # def build_filter_exception(self):
-    #     '''Ensures function returns output with at least 100 rows.'''
-    #     self.assertRaises(Exception, int_func.build_type_interact, building_category="None", data_directory=DATA_DIRECTORY)  #pylint: disable=line-too-long
+    def test_c_severity_interact(self):
+        """todo: write doc string"""
+        try:
+            int_func.collision_severity_interact(collision_severity="All", data_directory=DATA_DIRECTORY) #pylint: disable=line-too-long
+        except RuntimeError:
+            self.fail("collision_severity_interact raised ExceptionType unexpectedly.")
+
+    def test_collision_type_interact(self):
+        """todo: write doc string"""
+        try:
+            int_func.collision_type_interact(collision_type="All", data_directory=DATA_DIRECTORY) #pylint: disable=line-too-long
+        except RuntimeError:
+            self.fail("collision_type_interact raised ExceptionType unexpectedly.")
+
+    def test_radius_interact(self):
+        """todo: write doc string"""
+        try:
+            int_func.radius_interact(radius_from_building=1500, data_directory=DATA_DIRECTORY) #pylint: disable=line-too-long
+        except RuntimeError:
+            self.fail("radius_interact raised Exception unexpectedly.")
+
+    def test_all_factor_interact(self):
+        """todo: write doc string"""
+        try:
+            int_func.all_factor_interact(building_category='All', building_year=2016, collision_interval=12, collision_severity='All', collision_type='All', radius_from_building=1500, data_directory=DATA_DIRECTORY) #pylint: disable=line-too-long
+        except RuntimeError:
+            self.fail("all_factor_interact raised ExceptionType unexpectedly.")
 
     def test_build_invalid_path_error(self):
         '''Tests whether an invalid path raises a ValueError exception.'''
         self.assertRaises(ValueError, int_func.build_type_interact, building_category="All", data_directory='badPath') #pylint: disable=line-too-long
 
-    # def year_filter_exception(self):
-    #     '''Ensures function returns output with at least 100 rows.'''
-    #     self.assertRaises(Exception, int_func.year_int_interact, building_year=2010, collision_interval=0, data_directory=DATA_DIRECTORY) #pylint: disable=line-too-long
-
     def test_year_invalid_path_error(self):
         '''Tests whether an invalid path raises a ValueError exception.'''
         self.assertRaises(ValueError, int_func.year_int_interact, building_year=2015, collision_interval=12, data_directory='badPath') #pylint: disable=line-too-long
+
+    def test_c_s_invalid_path_error(self):
+        '''Tests whether an invalid path raises a ValueError exception.'''
+        self.assertRaises(ValueError, int_func.collision_severity_interact, collision_severity="All", data_directory='badPath') #pylint: disable=line-too-long
+
+    def test_c_t_invalid_path_error(self):
+        '''Tests whether an invalid path raises a ValueError exception.'''
+        self.assertRaises(ValueError, int_func.collision_type_interact, collision_type='All', data_directory='badPath') #pylint: disable=line-too-long
+
+    def test_radius_invalid_path_error(self):
+        '''Tests whether an invalid path raises a ValueError exception.'''
+        self.assertRaises(ValueError, int_func.radius_interact, radius_from_building=1500, data_directory='badPath') #pylint: disable=line-too-long
+
+    def test_a_f_invalid_path_error(self):
+        '''Tests whether an invalid path raises a ValueError exception.'''
+        self.assertRaises(ValueError, int_func.all_factor_interact, building_category='All', building_year=2016, collision_interval=12, collision_severity='All', collision_type='All', radius_from_building=1500, data_directory='badPath') #pylint: disable=line-too-long
 
 
 SUITE = unittest.TestLoader().loadTestsFromTestCase(TestInteractFunctions)
